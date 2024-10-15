6e7c0e51919796607212c6a74b62486eb92ee9d4
Toghrul <toghrulrahimli.04@gmail.com>
2024-10-15 21:43:13 +0800

    test: Add and Pass all move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -38,6 +38,7 @@ namespace adas
                     --pose.y;
                 }
             }
+            
         }
     }
 }
--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -23,11 +23,95 @@ namespace adas
         ASSERT_EQ(target, executor->Query());
     }
 
-    TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E) // standard test comes till here inclusevly
     {
         std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
         executor->Execute("M");
         const Pose target({1, 0, 'E'});
         ASSERT_EQ(target, executor->Query());
     }
+    TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W) // when face W, M shoudl make pose.x - 1
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+        executor->Execute("M");
+        const Pose target({-1, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N) // when face is N, M should make pose.y + 1
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+        executor->Execute("M");
+        const Pose target({0, 1, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S) // when face is S, M should make pose.y - 1
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+        executor->Execute("M");
+        const Pose target({0, -1, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    // TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E) // if face is E, turn L results in face N
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("L");
+    //     const Pose target({0, 0, 'N'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
+    // TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+    // {
+    //     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    //     executor->Execute("M");
+    //     const Pose target({1, 0, 'E'});
+    //     ASSERT_EQ(target, executor->Query());
+    // }
 }
