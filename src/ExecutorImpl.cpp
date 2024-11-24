54e51bb08eca50021d6d60de7deaaee5817ad681
Toghrul <toghrulrahimli.04@gmail.com>
2024-11-24 18:42:38 +0800

    initialization semantics to make the cmderMap init code more concise

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -21,11 +21,12 @@ namespace adas
     {
         for (const auto cmd : commands)
         {
-            std::unordered_map<char, std::function<void(PoseHandler & PoseHandler)>> cmderMap;
-            cmderMap.emplace('M', MoveCommand());
-            cmderMap.emplace('L', TurnLeftCommand());
-            cmderMap.emplace('R', TurnRightCommand());
-            cmderMap.emplace('F', FastCommand());
+            std::unordered_map<char, std::function<void(PoseHandler & PoseHandler)>> cmderMap{
+                {'M', MoveCommand()}, // calls default constructor for the object
+                {'L', TurnLeftCommand()},
+                {'R', TurnRightCommand()},
+                {'F', FastCommand()}};
+        
             // cmderMap.emplace('B', std::make_unique<ReverseCommand>);
             // std::unique_ptr<ICommand> cmder;
             const auto it = cmderMap.find(cmd); // find return pair<const Key, T> type if it finds the key, and unordered_map::end if it does not(end is element past the end of container)
@@ -33,6 +34,7 @@ namespace adas
             {
                 it->second(posehandler); // second indicates mapped value
             } // second actually points to the object itself
+            // in the case if it was MoveCommand(), it will call object(), which is a real operator usage for object
         }
     }
 }
